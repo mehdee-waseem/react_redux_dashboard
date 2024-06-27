73201648dad2146750c9da9a7da7a45f0adfcd47
mehdeewaseem <Muhammad Waseem>
2024-06-27 19:13:58 +0500

    New branch for redux dashboard

---
+++ src/components/Dasboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Product from "./Product";
+const Dasboard = () => {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+};
+
+export default Dasboard;
---
+++ src/components/Product.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { add } from "../store/cartSlice";
+import { getProducts } from "../store/productSlice";
+
+const Product = () => {
+  const dispatch = useDispatch();
+  const { data: products } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(getProducts());
+    //   fetch("https://fakestoreapi.com/products")
+    //     .then((response) => response.json())
+    //     .then((data) => setProducts(data));
+  }, [dispatch]);
+
+  const addToCart = (product) => {
+    dispatch(add(product));
+  };
+
+  const cards = products.map((product) => (
+    <div className="col-md-3" style={{ marginBottom: "10px" }} key={product.id}>
+      <Card className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: "100px", height: "130px" }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>PKR.{product.price}</Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <Button variant="primary" onClick={() => addToCart(product)}>
+            Add to Cart
+          </Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Product Dashboard</h1>
+      <div className="row">{cards}</div>
+    </div>
+  );
+};
+
+export default Product;
